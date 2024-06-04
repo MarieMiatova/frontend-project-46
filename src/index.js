6b0f0c1efc0acc2e0746f197f2537b661858e854
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2024-06-04 10:32:43 +0000

    Intial commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
-import path from 'node:path';
-import { readFileSync } from 'node:fs';
+import { readFileSync } from 'fs';
+import path from 'path';
 import parse from './parsers.js';
-import formattedFile from './formatters/index.js';
-import getDiffTree from './difftree.js';
+import getDifferenceTrees from './difftree.js';
+import formatter from './formatters/index.js';
 
-const getFullPath = (file) => path.resolve(process.cwd(), file);
+const getFixturesPath = (filename) => path.resolve(process.cwd(), filename);
+const getExtantion = (filename) => path.extname(filename).slice(1);
 
-const readFile = (file) => {
-  const filePath = getFullPath(file);
-  return readFileSync(filePath, 'utf8');
+const getData = (filepath) => parse(readFileSync(getFixturesPath(filepath), 'utf-8'), getExtantion(filepath));
+
+export default (filepath1, filepath2, format = 'stylish') => {
+  const arrObject = getDifferenceTrees(getData(filepath1), getData(filepath2));
+
+  return formatter(arrObject, format);
 };
-
-const getExtension = (file) => path.extname(file).slice(1);
-
-const getContent = (file) => {
-  const extension = getExtension(file);
-
-  return parse(readFile(file), extension);
-};
-
-const genDiff = (file1, file2, format = 'stylish') => {
-  const contentFile1 = getContent(file1);
-  const contentFile2 = getContent(file2);
-  const diffTree = getDiffTree(contentFile1, contentFile2);
-
-  return formattedFile(diffTree, format);
-};
-
-export default genDiff;
