a7d918df139c434408b2c6b3756200b21dac804f
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2024-06-04 08:24:27 +0000

    Initial commit

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,69 +1,22 @@
-import { readFileSync } from 'node:fs';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { jest } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { test, expect } from '@jest/globals';
+import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const testCases = [
-  [
-    'file1.json',
-    'file2.json',
-    'stylish',
-    'result_stylish.txt',
-  ],
-  [
-    'file1.json',
-    'file2.json',
-    'plain',
-    'result_plain.txt',
-  ],
-  [
-    'file1.json',
-    'file2.json',
-    'json',
-    'result_json.json',
-  ],
-  [
-    'file1.yml',
-    'file2.yml',
-    'stylish',
-    'result_stylish.txt',
-  ],
-  [
-    'file1.yml',
-    'file2.yml',
-    'plain',
-    'result_plain.txt',
-  ],
-  [
-    'file1.yml',
-    'file2.yml',
-    'json',
-    'result_json.json',
-  ],
-];
-
-test.each(testCases)('gendiff $format', (file1, file2, format, expected) => {
-  expect(
-    genDiff(
-      getFixturePath(file1),
-      getFixturePath(file2),
-      format,
-    ),
-  ).toEqual(readFile(expected).trim());
-});
-
-test('should use "stylish" formatter by default', () => {
-  const mockFormat = jest.fn();
-
-  genDiff(getFixturePath('file1.json'), getFixturePath('file1.json'), mockFormat());
-
-  expect(mockFormat).toHaveBeenCalledWith();
+test.each([
+  ['file1.json', 'file2.json', 'result_stylish.txt', 'stylish'],
+  ['file1.yaml', 'file2.yaml', 'result_stylish.txt', 'stylish'],
+  ['file1.json', 'file2.json', 'result_plain.txt', 'plain'],
+  ['file1.yaml', 'file2.yaml', 'result_plain.txt', 'plain'],
+  ['file1.json', 'file2.json', 'result_json.txt', 'json'],
+  ['file1.yaml', 'file2.yaml', 'result_json.txt', 'json'],
+])('formatters comparison', (file1, file2, testFile, format) => {
+  expect(genDiff(getFixturePath(file1), getFixturePath(file2), format)).toEqual(readFile(testFile));
 });
