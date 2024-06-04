8764384e56a9e69477555e95232893a268f54820
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2024-06-04 09:56:14 +0000

    Initial commit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,15 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { program } from 'commander';
+import genDiff from '../src/index.js';
 
-
-
-const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .helpOption('-h, --help', 'output usage information')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
   .action((filepath1, filepath2) => {
-    const result = genDiff(filepath1, filepath2, program.opts().format);
-    console.log(result);
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
+
 program.parse();
